2d0e9d840c0c4734e3efeda80978c6c9c2dfa68f
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-09-29 01:20:47 +0200

    Add supabase backend

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hiioqbztscyvmcavphxb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaW9xYnp0c2N5dm1jYXZwaHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MzcyNzgsImV4cCI6MjAxMTUxMzI3OH0.kiWhuR4qGpjmM4XDyVlYz9QhHEhV7Eoic5-ibEnwXT4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
